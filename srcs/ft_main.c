43cec46f327fcc130444ba233b818a419713ba59
Geoffroy Barnay <gbarnay@z2r6p1.le-101.fr>
2018-06-25 01:15:07 +0200

    shared memory with childs

--- srcs/ft_main.c
+++ srcs/ft_main.c
@@ -12,6 +12,10 @@
 /* ************************************************************************** */
 
 #include "taskmaster.h"
+#include <sys/mman.h>
+#include <signal.h>
+//#include <pthread.h>
+//#include <sched.h>
 
 typedef struct		s_keyval
 {
@@ -45,19 +49,27 @@ typedef struct		s_status
 	enum {
 					stopped,
 					running
-	};
+	}				state;
 	int				started_time;
 	int				stopped_time;
 }					t_status;
 
+typedef struct		s_shared
+{
+	t_status		status[500];
+}					t_shared;
+
 typedef struct		s_tm
 {
 	t_job			jobs[1000];
-	t_status		status[1000];
+//	t_status		status[1000];
+//	t_status		*status;
+	t_shared		*shared;
 	int				jobs_cnt;
 	char			**env;
 	char			*cmd;
 	pid_t			jobs_watcher;
+	
 }					t_tm;
 
 void	ft_init_job(t_job *job)
@@ -222,32 +234,89 @@ void	ft_parse_config(t_tm *tm, char *config_file)
 //------------------------------------------------------------------------------
 void	ft_process_watcher(t_tm *tm)
 {
-	pid_t	pid;
+//	pid_t	pid;
 	int		i;
-int	cnt = -1;
+	int		res;
 
+//while (1)
+//{
 	i = -1;
-	while (++cnt < 5 && ++i < tm->jobs_cnt)
+	while (++i < tm->jobs_cnt)
 	{
-		pid = waitpid(tm->status[i].pid, NULL, WNOHANG|WUNTRACED);
-		if (pid == 0)
-			ft_printf("process: [%s] is still running.\n", tm->jobs[i].name);
-		else if (pid == -1)
-			ft_printf("What da fuk is going on ? c dla merde waitpid ou quoi ?\n");
+	//	pid = waitpid(tm->status[i].pid, NULL, WNOHANG|WUNTRACED);
+//		pid = waitpid(tm->shared->status[i].pid, NULL, WNOHANG|WUNTRACED);
+		if (!tm->shared->status[i].pid)
+			res = 99;
 		else
-			ft_printf("process: [%s] is not running.\n", tm->jobs[i].name);
-		sleep(1);
+			res = kill(tm->shared->status[i].pid, 0);
+		if (res == 0)
+		{
+	//		ft_printf("process: [%s] is still running.\n", tm->jobs[i].name);
+			tm->shared->status[i].state = running;
+			tm->shared->status[i].started_time = time(NULL);
+		}
+//		else if (res == -1)
+//		{
+			// maybe we should remove current pid or retry
+		//	ft_printf("waitpid error for process '%s' (pid %d)\n", tm->jobs[i].name, tm->status[i].pid);
+	//		ft_printf("waitpid error for process '%s' (pid %d)\n", tm->jobs[i].name, tm->shared->status[i].pid);
+//			ft_printf("process watcher error checking: [%s]\n", tm->jobs[i].name);
+//		}
+//		else if (res > 0)
+		else
+		{
+//			ft_printf("process: [%s] terminated.\n", tm->jobs[i].name);
+			tm->shared->status[i].state = stopped;
+			tm->shared->status[i].stopped_time = time(NULL);
+		}
+		sleep(3);
 	}
+//}
 }
 
 void	ft_start_process_watcher(t_tm *tm)
 {
-	ft_printf("Starting process watcher.\n");
-	ft_process_watcher(tm);
+	pid_t	father;
+//	t_status	*shared;
+//	pthread_t thread1;
+
+//	for (int cnt = 0; cnt < 5; cnt++)
+
+//	if(pthread_create(&thread1, NULL, ft_process_watcher(tm), NULL) == -1) {
+//		perror("pthread_create");
+
+//int shm_id = shmget(IPC_PRIVATE, sizeof(t_status), IPC_CREAT | 0666);
+//shm = shmat(shm_id, 0, 0);
+(void)tm;
+//shared = (t_status*)mmap(NULL, sizeof(t_status), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+//shared->state = 1;
+//ft_printf("Genial: [%d]\n", shared->state);
+
+	father = fork();
+	if (!father)
+	{
+		while (1)
+			ft_process_watcher(tm);
+//		exit(0);
+	}
+	else
+	{
+		ft_printf("Starting process watcher. [pid: %d]\n", father);
+	}
 }
 //------------------------------------------------------------------------------
+void	ft_debug_status(t_status *status)
+{
+	ft_printf("pid:\t[%d]\n", status->pid);
+	ft_printf("state:\t[%d]\n", status->state);
+	ft_printf("stime:\t[%d]\n", status->started_time);
+	ft_printf("ttime:\t[%d]\n", status->stopped_time);
+}
+
 void	ft_init_status(t_status *status)
 {
+	status->pid = 0;
+	status->state = stopped;
 	status->started_time = 0;
 	status->stopped_time = 0;
 }
@@ -280,10 +349,12 @@ void	ft_exec_job(t_tm *tm, int id_job)
 	pid_t	father;
 	char	**args;
 
-	ft_init_status(&tm->status[id_job]);
+//	ft_init_status(&tm->status[id_job]);
+ft_init_status(&tm->shared->status[id_job]);
 	father = fork();
 	if (!father)
 	{
+		
 		args = NULL;
 		args = ft_strsplit(tm->jobs[id_job].cmd, ' ');
 		if (args && execve(args[0], args, tm->env) < 0)
@@ -291,16 +362,35 @@ void	ft_exec_job(t_tm *tm, int id_job)
 		ft_free_list(args);
 		exit(0);
 	}
-//	if (father > 0)
+	if (father > 0)
+	{
+		tm->shared->status[id_job].pid = father;
+		ft_printf("process id: [%d]\n", father);
+	}
 }
 
 void	ft_get_job_status(t_tm *tm, int id_job)
 {
+/*	struct stat		fs;
+	char			*process_path;
+	
+	process_path = NULL;
+	ft_sprintf(&process_path, "/proc/%d", tm->shared->status[id_job].pid);
+	ft_printf("p_path: [%s]\n", process_path);
+	int res = stat(process_path, &fs);
+	ft_printf("res: [%d]\n", res);
+	ft_printf("omfg: [%s]\n", ctime(&fs.st_mtime));*/
+
 	// name RUNNING pid uptime
-	ft_printf("%-20s", tm->jobs[id_job].name);
-	ft_printf("%-10s", "RUNNING");
-	ft_printf("%-10s", "pid 365,");
-	ft_printf("%-10s\n", "uptime 0:05:03");
+//	ft_printf("state: [%d]\n", tm->status[id_job].state);
+	ft_printf("%s", tm->shared->status[id_job].state ? "\e[92m" : "\e[91m");
+	ft_printf("%-20s ", tm->jobs[id_job].name);
+	ft_printf("%-10s ", tm->shared->status[id_job].state ? "RUNNING" : "STOPPED");
+	ft_printf("pid %-10d", tm->shared->status[id_job].pid);
+//	ft_printf("%-10s\n", "uptime 0:05:03");
+	ft_printf("uptime %-10d\n", time(NULL) - tm->shared->status[id_job].started_time);
+	ft_printf("{eoc}");
+//	ft_printf("Uptime: ", time(NULL) - tm->status[id_job].stopped_time);
 }
 
 void	ft_cmd_start(t_tm *tm, char *name)
@@ -381,6 +471,8 @@ int		main(int argc, char *argv[], char *env[])
 {
 	t_tm	tm;
 
+	signal(SIGCHLD, SIG_IGN); // ignore fcking SIGCHLD so wait become useless and no more zombies
+	tm.shared = (t_shared*)mmap(NULL, sizeof(t_shared), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
 	tm.env = env;
 	tm.jobs_cnt = 0;
 	if (argc == 2)
@@ -394,9 +486,11 @@ int		main(int argc, char *argv[], char *env[])
 		{
 			ft_get_user_input(&tm);
 			ft_process_cmd(&tm);
+//			ft_start_process_watcher(&tm);
 		}
 	}
 	else
 		ft_printf("usage: taskmaster config_file\n");
+munmap(tm.shared, sizeof(t_shared));
 	return (0);
 }
