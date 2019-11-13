d7860ce2f63444fdd0a8e5088f21007cfad2b1cd
guyb27 <gmadec@z1r5p2.le-101.fr>
2019-11-13 03:03:56 +0100

    Encore un peu de propre

--- client/include/init.h
+++ client/include/init.h
@@ -6,7 +6,7 @@
 /*   By: gmadec <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/08/12 04:41:57 by gmadec       #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/02 01:42:59 by dzonda      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/13 03:00:49 by gmadec      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,15 +18,11 @@
 
 int			init_shell(int ac, const char **av);
 int			init_files(const char *path);
-int			init_env(const char *path);
 int			init_builtins(const char *bin);
 
 char		*concat_name_value(char *name, char *value);
 char		*search_path_of_101sh(const char *bin);
 
 int			index_to_var(char *name, char **env);
-char		*ft_envset_line(const char **envset, const char *name);
-char		*ft_envset_join(const char *name, const char *value);
-char		*ft_envset_value(const char **envset, const char *name);
 
 #endif
--- client/src/init/init_shell.c
+++ client/src/init/init_shell.c
@@ -6,18 +6,40 @@
 /*   By: dzonda <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/09/13 22:58:19 by dzonda       #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/19 09:17:42 by gmadec      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/13 03:02:51 by gmadec      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "heart.h"
 
+char		*ft_envset_value(const char **envset, const char *name)
+{
+	int		i;
+	char	*tmp;
+	char	*s;
+
+	i = -1;
+	tmp = NULL;
+	s = NULL;
+	while (envset[++i])
+	{
+		tmp = ft_strsub(envset[i], 0, ft_strlen(name));
+		if (ft_strcmp(name, tmp) == 0 && envset[i][ft_strlen(name)] == '=')
+			s = ft_strdup(ft_strchr(envset[i], '=') + 1);
+		ft_strdel(&tmp);
+		if (s)
+			break ;
+	}
+	return (s);
+}
+
 int			ft_init_vars(int ac, const char **av)
 {
 	char	*path;
+	char		*term_var;
+	extern char	**environ;
 
-	path = NULL;
 	if (!(path = search_path_of_101sh(av[0])))
 		return (EXIT_FAILURE);
 	if (init_files(path))
@@ -25,11 +47,12 @@ int			ft_init_vars(int ac, const char **av)
 		ft_strdel(&path);
 		return (EXIT_FAILURE);
 	}
-	if (init_env(path))
-	{
-		ft_strdel(&path);
+	if (!(term_var = ft_envset_value((const char **)environ, "TERM")))
+		if (!(term_var = ft_strdup("xterm-256color")))
+			return (EXIT_FAILURE);
+	if (tgetent(NULL, term_var) == ERR)
 		return (EXIT_FAILURE);
-	}
+	ft_strdel(&term_var);
 	if (init_builtins((const char *)path))
 	{
 		ft_strdel(&path);
@@ -37,12 +60,6 @@ int			ft_init_vars(int ac, const char **av)
 	}
 	ft_strdel(&path);
 	return (EXIT_SUCCESS);
-}
-
-void		ft_init_signals(void)
-{
-	signal(SIGINT, ft_handle_sigint);
-	signal(SIGWINCH, ft_handle_resize);
 }
 
 int			init_shell(int ac, const char **av)
@@ -53,7 +70,8 @@ int			init_shell(int ac, const char **av)
 	{
 		while (tcgetpgrp(STDIN_FILENO) != (shell_pgid = getpgrp()))
 			kill(-shell_pgid, SIGTTIN);
-		ft_init_signals();
+		signal(SIGINT, ft_handle_sigint);
+		signal(SIGWINCH, ft_handle_resize);
 		shell_pgid = getpid();
 		tcsetpgrp(STDIN_FILENO, shell_pgid);
 		ft_init_vars(ac, av);
